152926c1cf4c43c1f6478682f3e47b8b3ba04557
joey <j.tan2231@gmail.com>
2023-02-10 14:35:13 -0500

    spectral clustering algorithm

--- api/engine/cluster.py
+++ api/engine/cluster.py
@@ -4,7 +4,9 @@ import numpy as np
 from math import sqrt
 from api.models import Cluster, Embedding, ClusterLink, ClusteringAlgorithm
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering
+
+CLUSTERING_ALGORITHM = SpectralClustering
 
 # TODO: this has a lot of optimization opportunities
 def create_clusters(user_id, entry_count, new_embeddings, new_entry_id):
@@ -21,11 +23,13 @@ def create_clusters(user_id, entry_count, new_embeddings, new_entry_id):
         no_algo = True
 
     count = entry_count
-    new_cluster_count = int(sqrt(count))
+    new_cluster_count = 2*int(sqrt(count))
     print(new_cluster_count)
 
-    # check if new clusters need made
-    if new_cluster_count == cluster_count:
+    # check if we can stick with the same number of clusters
+    #if new_cluster_count == cluster_count:
+    # NOTE: only true if KMeans because KMeans is the only clustering algorithm with a predict() function
+    if CLUSTERING_ALGORITHM == KMeans and new_cluster_count == cluster_count:
         clusterer = pickle.loads(clustering_algorithm.algorithm)
         new_average = np.mean(new_embeddings, axis=0, keepdims=True)
         labels = clusterer.predict(new_average)
@@ -35,8 +39,7 @@ def create_clusters(user_id, entry_count, new_embeddings, new_entry_id):
         new_cluster_link = ClusterLink(cluster_id=mapping[labels[0]], entry_id=new_entry_id, user_id=user_id)
         new_cluster_link.save()
 
-        return
-
+    # clusters need regenerated
     else:
         try:
             # generate entry-average embeddings for each of the user's entries
@@ -48,56 +51,56 @@ def create_clusters(user_id, entry_count, new_embeddings, new_entry_id):
                 if avg is None:
                     average_dict[entry_id] = [pickle.loads(embedding.embedding), 1]
                 else:
-                    average_dict[entry_id] = [avg[0] + pickle.loads(embedding.embedding), avg[1]]
+                    average_dict[entry_id] = [avg[0] + pickle.loads(embedding.embedding), avg[1] + 1]
 
             averages = [v[0]/v[1] for _, v in average_dict.items()]
-            averages = [a / np.linalg.norm(a) for a in averages] # normalization step -- is this needed?
+            #averages = [a / np.linalg.norm(a) for a in averages] # normalization step -- is this needed?
         except Embedding.DoesNotExist:
             return
 
-    print(f'creating new clustering algorithm for refitting for {user_id}')
-    clusterer = KMeans(n_clusters=new_cluster_count)
-    pickled = pickle.dumps(clusterer)
+        print(f'creating new clustering algorithm for refitting for {user_id}')
+        clusterer = SpectralClustering(n_clusters=new_cluster_count)
+        pickled = pickle.dumps(clusterer)
 
-    if no_algo:
-        clustering_algorithm = ClusteringAlgorithm(user_id=user_id, algorithm=pickled)
+        if no_algo:
+            clustering_algorithm = ClusteringAlgorithm(user_id=user_id, algorithm=pickled)
+            clustering_algorithm.save()
+            print('clustering algorithm created')
+
+        print('refitting clustering algorithm')
+        clusterer = clusterer.fit(averages)
+
+        print('refitted')
+        clustering_algorithm.algorithm = pickle.dumps(clusterer)
+
+        entry_ids = list(average_dict.keys())
+        labels = clusterer.labels_
+
+        assert len(entry_ids) == len(averages)
+
+        clustered_ids = { label: [] for label in labels }
+        for i in range(len(entry_ids)):
+            clustered_ids[labels[i]].append(entry_ids[i])
+
+        # clear out old clusters in database to make room for new ones
+        ClusterLink.objects.filter(user=user_id).delete()
+        Cluster.objects.filter(user=user_id).delete()
+
+        # create clusters in DB
+        new_clusters = [Cluster(user_id=user_id, title='untitled') for _ in range(new_cluster_count)]
+        new_clusters = Cluster.objects.bulk_create(new_clusters)
+
+        new_cluster_links = []
+        for k in clustered_ids.keys():
+            for id in clustered_ids[k]:
+                new_cluster_links.append(ClusterLink(
+                    cluster=new_clusters[k],
+                    entry_id=id,
+                    user_id=user_id,
+                ))
+
+        new_cluster_links = ClusterLink.objects.bulk_create(new_cluster_links)
+
+        cluster_id_mapping = { label: new_clusters[label].pk for label in labels }
+        clustering_algorithm.cluster_id_mapping = pickle.dumps(cluster_id_mapping)
         clustering_algorithm.save()
-        print('clustering algorithm created')
-
-    print('refitting clustering algorithm')
-    clusterer = clusterer.fit(averages)
-
-    print('refitted')
-    clustering_algorithm.algorithm = pickle.dumps(clusterer)
-
-    entry_ids = list(average_dict.keys())
-    labels = clusterer.labels_
-
-    assert len(entry_ids) == len(averages)
-
-    clustered_ids = { label: [] for label in labels }
-    for i in range(len(entry_ids)):
-        clustered_ids[labels[i]].append(entry_ids[i])
-
-    # clear out old clusters in database to make room for new ones
-    ClusterLink.objects.filter(user=user_id).delete()
-    Cluster.objects.filter(user=user_id).delete()
-
-    # create clusters in DB
-    new_clusters = [Cluster(user_id=user_id, title='untitled') for _ in range(new_cluster_count)]
-    new_clusters = Cluster.objects.bulk_create(new_clusters)
-
-    new_cluster_links = []
-    for k in clustered_ids.keys():
-        for id in clustered_ids[k]:
-            new_cluster_links.append(ClusterLink(
-                cluster=new_clusters[k],
-                entry_id=id,
-                user_id=user_id,
-            ))
-
-    new_cluster_links = ClusterLink.objects.bulk_create(new_cluster_links)
-
-    cluster_id_mapping = { label: new_clusters[label].pk for label in labels }
-    clustering_algorithm.cluster_id_mapping = pickle.dumps(cluster_id_mapping)
-    clustering_algorithm.save()
